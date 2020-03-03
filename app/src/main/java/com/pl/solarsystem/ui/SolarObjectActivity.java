fe294bb7bbcbed9970325551f60058c88a56126c
Jarosław Zieliński <jaroslawzielinski1986@wp.pl>
2020-03-03 20:55:08 +0100

    Added moons in Solarobject

--- app/src/main/java/com/pl/solarsystem/ui/SolarObjectActivity.java
+++ app/src/main/java/com/pl/solarsystem/ui/SolarObjectActivity.java
@@ -11,34 +11,43 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.Html;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.pl.solarsystem.R;
 import com.pl.solarsystem.model.SolarObject;
+import com.pl.solarsystem.ui.solarObject.SolarObjectsAdapter;
 
 import java.io.IOException;
 import java.io.Serializable;
 
-public class SolarObjectActivity extends AppCompatActivity {
+public class SolarObjectActivity extends AppCompatActivity implements SolarObjectsAdapter.SolarObjectClickedListener {
 
     public static final String OBJECT = "object";
     private SolarObject solarObject;
     private TextView textView;
     private ImageView imageView;
     private CollapsingToolbarLayout collapsingToolbarLayout;
+    private RecyclerView moonsRecyclerView;
+    private TextView moonsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solar_object);
-        textView=findViewById(R.id.objectTextView);
-        imageView=findViewById(R.id.objectImageView);
+        textView = findViewById(R.id.objectTextView);
+        imageView = findViewById(R.id.objectImageView);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        collapsingToolbarLayout=findViewById(R.id.toolbar_layout);
+        collapsingToolbarLayout = findViewById(R.id.toolbar_layout);
+        moonsRecyclerView = findViewById(R.id.moonsRecyclerView);
+        moonsTextView = findViewById(R.id.moonsLabel);
 
         setSupportActionBar(toolbar);
 
@@ -53,9 +62,9 @@ public class SolarObjectActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         solarObject = (SolarObject) getIntent().getSerializableExtra(OBJECT);
         collapsingToolbarLayout.setTitle(solarObject.getName());
-        String text= null;
+        String text = null;
         try {
-            text = solarObject.loadStringFromAssets(this,solarObject.getText());
+            text = solarObject.loadStringFromAssets(this, solarObject.getText());
             textView.setText(Html.fromHtml(text));
         } catch (IOException e) {
             e.printStackTrace();
@@ -64,12 +73,26 @@ public class SolarObjectActivity extends AppCompatActivity {
         Glide.with(this)
                 .load(solarObject.getImagePath())
                 .into(imageView);
+        moonsRecyclerView.setVisibility(solarObject.hasMoons() ? View.VISIBLE : View.GONE);
+        moonsTextView.setVisibility(solarObject.hasMoons() ? View.VISIBLE : View.GONE);
+        if (solarObject.hasMoons()) {
+            SolarObjectsAdapter solarObjectsAdapter = new SolarObjectsAdapter(solarObject.getMoons());
+            solarObjectsAdapter.setSolarObjectClickedListener(this);
+            moonsRecyclerView.setAdapter(solarObjectsAdapter);
+            moonsRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+            moonsRecyclerView.setNestedScrollingEnabled(false);
+        }
     }
-    public static void startActivity(Activity activity,SolarObject solarObject){
-        Intent intent=new Intent(activity, SolarObjectActivity.class);
+
+    public static void startActivity(Activity activity, SolarObject solarObject) {
+        Intent intent = new Intent(activity, SolarObjectActivity.class);
         intent.putExtra(OBJECT, solarObject);
         activity.startActivity(intent);
     }
 
 
+    @Override
+    public void solarObjectClicked(SolarObject solarObject) {
+        SolarObjectActivity.startActivity(this, solarObject);
+    }
 }
